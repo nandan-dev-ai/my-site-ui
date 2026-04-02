f6d145ac23cef3217961a9edf0defb9c27b6bdaf
Nandan <nbarman760@gmail.com>
2026-04-02 10:40:34 +0530

    update axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -18,7 +18,7 @@ api.interceptors.request.use(
         config.headers = {
           ...config.headers,
           Authorization: `Bearer ${token}`,
-        };
+        } as any;
       }
     }
     return config;
